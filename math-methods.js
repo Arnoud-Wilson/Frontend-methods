670f59fc2b2a8af5a3b27d4691b5c52a993638e9
Arnoud-Wilson <arnoud608@gmail.com>
2023-01-23 20:32:39 +0100

    Math methods afgemaakt

--- math-methods.js
+++ math-methods.js
@@ -9,18 +9,21 @@ console.log(Math.PI);
 // Tip: de formule is omtrek = diameter * pi
 // ---- Verwachte uitkomst: 12.566370614359172
 
+console.log(Math.PI * 4);
 
 /* Opdracht 2: */
 // Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
 
+const circumference= Math.PI * 12;
 
+console.log(Math.round(circumference));
 
 /* Opdracht 3: */
 // Bereken de wortel van 9 en log dit in de terminal
 // ---- Verwachte uitkomst: 3
 
-
+console.log(Math.sqrt(9));
 
 /* Opdracht 4: */
 // Genereer een willekeurig getal tussen de 0 en 1 en doe dan een check:
@@ -28,3 +31,14 @@ console.log(Math.PI);
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
 // Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+
+const number= Math.random();
+
+console.log(number);
+
+if (number > 0.5) {
+    console.log("Het glas is halfvol");
+}
+else {
+    console.log("Het glas is half leeg")
+}
